38a081c2bcaeac3a1957b878960182f405b58b86
Bigset09 <derikossman6@gmail.com>
2025-04-15 03:34:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; const root = ReactDOM.createRoot(document.getElementById('root')); root.render(<App />);
+                                              if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registrado com sucesso:", registration);
+    })
+    .catch((error) => {
+      console.log("Falha ao registrar o Service Worker:", error);
+    });
+}
+                                                                                                                              
